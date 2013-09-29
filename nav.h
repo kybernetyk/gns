7ebb1702efaf899958101641a9284b043add1c6d
kyb <leon@celestialmachines.com>
2013-09-29 10:42:42 +0200

    katzenfutter is best futter

--- nav.h
+++ nav.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace nav {
-
-
+	float distance_between(float lat1, float lon1, float lat2, float lon2);
+	float heading_fromto(float lat1, float lon1, float lat2, float lon2);
 }

1d09602885aacda6dd9051feb8b5e6d1d3640bfd
kyb <leon@celestialmachines.com>
2013-10-04 20:36:12 +0200

    autoconfig os

--- config.h
+++ config.h
@@ -1,11 +1,14 @@
 #pragma once
 
-//os x, 2nd usb port
-#define DEVICE_PATH "/dev/cu.PL2303-00001004"
 
-//linux
-//#define DEVICE_PATH "/dev/ttyUSB0"
-
+#ifdef __APPLE__
+	//os x, 2nd usb port
+	#define DEVICE_PATH "/dev/cu.PL2303-00001004"
+#else
+	//linux
+	#define DEVICE_PATH "/dev/ttyUSB0"
+#endif
+	
 //destination coordinates
 #define DEST_LAT 51.202499f
 #define DEST_LON 6.410629f

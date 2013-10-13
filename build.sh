591784e409956a0ff58ed4b653ed899f7e790bfb
Leon <leon@celestialmachines.com>
2013-10-13 14:21:46 +0200

    build now respects exit codes

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # this is better than make and friends!
 
-clang -c termbox/src/*.c
-clang++ -I/usr/include/c++/v1/ -std=c++11 -stdlib=libc++ -lpthread -lc++ -o gns *.cpp *.o
-rm *.o
+clang -c termbox/src/*.c && clang++ -I/usr/include/c++/v1/ -std=c++11 -stdlib=libc++ -lpthread -lc++ -o gns *.cpp *.o && rm *.o
